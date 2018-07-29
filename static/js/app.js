da52a02a3e48b310c006cfdb02ea5fba507c0027
Brian Connell <brianconnell@brians-mbp.attlocal.net>
2018-07-28 23:44:28 -0500

    added dropdowns

--- static/js/app.js
+++ static/js/app.js
@@ -1,89 +1,99 @@
-var output = d3.select('.myOutput');
 
 // Get a reference to the table body
 var tbody = d3.select("tbody");
 
-// Prevent the page from refreshing
-//d3.event.preventDefault();
+var row = tbody.append("tr");
 
-var inputField = d3.select('#datetime');
+var cell = tbody.append("td");
 
 //dropdowns
-var dateDropdown = d3.select('#dateDropdown')
-//console.log("selectedDate",selectedDate)
+var dateDropdown = d3.select('#dateDropdown');
+var cityDropdown = d3.select('#cityDropdown');
+var stateDropdown = d3.select('#stateDropdown');
+var countryDropdown = d3.select('#countryDropdown');
+var shapeDropdown = d3.select('#shapeDropdown');
 
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
-function update(tableData){
-  //d3.select("tbody").html("")
-  var row = tbody.append("tr");
 
-  tableData.forEach(function (datum) {
-    var row = tbody.append("tr");
-    //console.log('row', row);
-    Object.entries(datum).forEach(([key, value]) => {
-      var cell = tbody.append("td");
-      //cell.text(value);
-    });
-  });
-
-}
-
-function myInit() {
-  console.log(tableData)
-
-  //d3.select("tbody").html("")
-
-  tableData.forEach(function (datum) {
-    var row = tbody.append("tr");
-    //console.log('row', row);
-    Object.entries(datum).forEach(([key, value]) => {
-      var cell = tbody.append("td");
-      //cell.text(value);
-    });
-  });
-}
-
-myInit()
-
-// Input fields can trigger a change event when new text is entered.
-// inputField.on("change", function () {
-//   var newText = d3.event.target.value;
-//   console.log(newText);
-// });
-
-dateDropdown.on("change", function() {
-  selected_date= d3.event.target.value;
-  tableData=tableData.filter(row =>row.datetime === selected_date);
-  update(tableData);
+// Refresh
+var submit = d3.select("#submit");
+submit.on("click", function () {
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
 })
 
-// function getCountryData(value){
-//   temp = tableData.filter(row =>{
-//     row.forEach(d=>d.country===value)
-//   })
-//   console.log(temp)
-// }
+
+function update(filteredData) {
+  d3.select("tbody").html("");
+  filteredData.forEach((datum) => {
+    var row = tbody.append("tr");
+    Object.entries(datum).forEach(([key, value]) => {
+      var cell = tbody.append("td");
+      cell.text(value);
+    });
+  });
+};
+
+
+function getDateData(value) {
+  let temp = tableData.filter(row => row.datetime === value);
+
+  if (temp) {
+    update(temp);
+  }
+}
+
+function getCityData(value) {
+  let temp = tableData.filter(row => row.city === value);
+
+  if (temp) {
+    update(temp);
+  }
+}
+
+function getStateData(value) {
+  let temp = tableData.filter(row => row.state === value);
+
+  if (temp) {
+    update(temp);
+  }
+}
+
+function getCountryData(value) {
+  let temp = tableData.filter(row => row.country === value);
+
+  if (temp) {
+    update(temp);
+  }
+}
+
+function getShapeData(value) {
+  let temp = tableData.filter(row => row.shape === value);
+
+  if (temp) {
+    update(temp);
+  }
+
+}
 
 
 dates = [];
 cities = [];
-states= [];
+states = [];
 countries = [];
 shapes = [];
 
-
-var dateData  = data.forEach(d => {dates.push(d.datetime)});
-var cityData = data.forEach(d => {cities.push(d.city)});
-var stateData = data.forEach(d => {states.push(d.state)}); 
-var countryData = data.forEach(d => {countries.push(d.country)});
-var shapeData = data.forEach(d => {shapes.push(d.shape)});
-
-function onlyUnique(value, index, self) { 
+function onlyUnique(value, index, self) {
   return self.indexOf(value) === index;
 }
+
+var dateData = data.forEach(d => { dates.push(d.datetime) });
+var cityData = data.forEach(d => { cities.push(d.city) });
+var stateData = data.forEach(d => { states.push(d.state) });
+var countryData = data.forEach(d => { countries.push(d.country) });
+var shapeData = data.forEach(d => { shapes.push(d.shape) });
 
 var dateOptions = dates.filter(onlyUnique);
 var cityOptions = cities.filter(onlyUnique);
@@ -91,7 +101,7 @@ var stateOptions = states.filter(onlyUnique);
 var countryOptions = countries.filter(onlyUnique);
 var shapeOptions = shapes.filter(onlyUnique);
 
-// console.log(dateOptions, cityOptions, stateOptions, countryOptions,shapeOptions)
+console.log(dateOptions, cityOptions, stateOptions, countryOptions, shapeOptions)
 
 // populate dates drop-down
 d3.select("#dateDropdown")
@@ -99,17 +109,17 @@ d3.select("#dateDropdown")
   .data(dateOptions)
   .enter()
   .append("option")
-  .attr("value", function(option) { return option; })
-  .text(function(option) { return option; });
+  .attr("value", function (option) { return option; })
+  .text(function (option) { return option; });
 
-  // populate dates drop-down
+// populate cities drop-down
 d3.select("#cityDropdown")
-.selectAll("option")
-.data(cityOptions)
-.enter()
-.append("option")
-.attr("value", function(option) { return option; })
-.text(function(option) { return option; });
+  .selectAll("option")
+  .data(cityOptions)
+  .enter()
+  .append("option")
+  .attr("value", function (option) { return option; })
+  .text(function (option) { return option; });
 
 // populate state drop-down
 d3.select("#stateDropdown")
@@ -117,23 +127,37 @@ d3.select("#stateDropdown")
   .data(stateOptions)
   .enter()
   .append("option")
-  .attr("value", function(option) { return option; })
-  .text(function(option) { return option; });
+  .attr("value", function (option) { return option; })
+  .text(function (option) { return option; });
 
-  // populate country drop-down
+// populate country drop-down
 d3.select("#countryDropdown")
-.selectAll("option")
-.data(countryOptions)
-.enter()
-.append("option")
-.attr("value", function(option) { return option; })
-.text(function(option) { return option; });
+  .selectAll("option")
+  .data(countryOptions)
+  .enter()
+  .append("option")
+  .attr("value", function (option) { return option; })
+  .text(function (option) { return option; });
 
- // populate shapes drop-down
- d3.select("#countryDropdown")
- .selectAll("option")
- .data(shapeOptions)
- .enter()
- .append("option")
- .attr("value", function(option) { return option; })
- .text(function(option) { return option; });
+// populate shapes drop-down
+d3.select("#shapeDropdown")
+  .selectAll("option")
+  .data(shapeOptions)
+  .enter()
+  .append("option")
+  .attr("value", function (option) { return option; })
+  .text(function (option) { return option; });
+
+
+
+function myInit() {
+  tableData.forEach((datum) => {
+    var row = tbody.append("tr");
+    Object.entries(datum).forEach(([key, value]) => {
+      var cell = tbody.append("td");
+      cell.text(value);
+    });
+  });
+}
+
+myInit()
